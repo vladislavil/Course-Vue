b2e9059cb8d990aabcfa9e3551e90dfcd135a75c
valdislav i <vladoss_19995@mail.ru>
2020-01-29 11:50:54 +0300

    init commit

---
+++ src/components/MyInput.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+    label {{name}}
+        i.fa( :class='vlaidClass', v-if='activated')
+        input(type='text', id='', :value='value', @input='onInput')
+</template>
+
+<script>
+export default {
+    props: ['name', 'value', 'pattern'],
+    data() {
+      return {
+        activated: this.value !== ""
+      }
+    },
+    computed: {
+        vlaidClass(){
+          return this.pattern.test(this.value) ?
+            'fa-check-circle' :
+            'fa-exclamation-circle';
+        }
+    },
+    methods: {
+      onInput(e) {
+          this.activated = true;
+          this.$emit('input', {
+            value: e.target.value,
+            valid: this.pattern.test(e.target.value)
+      })
+    }
+  }
+}
+</script>
