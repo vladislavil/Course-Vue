b7c1f3e1a9a35907b69c8c9df3fd9e741b138c02
valdislav i <vladoss_19995@mail.ru>
2020-01-29 16:29:53 +0300

    dz6

--- src/App.vue
+++ src/App.vue
@@ -1,103 +1,45 @@
 <template lang="pug">
-  div
-    form.form-group(action='', @submit.prevent='formSubmited = true', v-if='!formSubmited')
-      div.progress
-        div.progress-bar(role='progressbar', :style="'width:' + progressWidth + '%'", :aria-valuenow="progressWidth + '%'", aria-valuemin='0', aria-valuemax='100')
-      my-input(v-for='(item, index) in Info', :key='index', :name='item.name', :value='item.value', :pattern='item.pattern', @input='onInput(index, $event)')
-      button.btn.btn-primary(type='submit', :disabled='done < Info.length') Submit
-    table.table(v-if='formSubmited')
-      tr(v-for='(item, index) in Info', :key='index')
-        td {{ item.name }}
-        td {{ item.value }}
+  div.container
+    form.form-group
+      radio(v-for="(item, index) in List" :key="index" :type="item.type" :title="item.title" :answers="item.answers")
+      button.btn.btn-primary(type="submit") Send
+    table.table
+      tr
+        td
 </template>
 
 <script>
-
-import MyInput from './components/MyInput';
-
+import Radio from "./components/Radio";
 export default {
-  components: { MyInput },
+  components: { Radio },
   data() {
     return {
-      formSubmited: false,
-      done: 0,
-      controls: [],
-      Info: [
+      List: [
         {
-          name: 'Name',
-          value: '',
-          pattern: /^[a-zA-Z]{2,30}$/
+          type: "radio",
+          title: "Какой тег задает ссылку?",
+          answers: [
+            'a', 
+            'div', 
+            'span', 
+            'img'
+          ]
         },
         {
-          name: 'Phone',
-          value: '',
-          pattern: /^[0-9]{2,30}$/
-        },
-        {
-          name: 'Email',
-          value: '',
-          pattern: /.+/
-        },
-        {
-          name: 'Some field 1',
-          value: '',
-          pattern: /.+/
-        },
-        {
-          name: 'Some field 2',
-          value: '',
-          pattern: /.+/
+          type: "checkbox",
+          title: "Какой из этих тегов строчные?",
+          answers: [
+            'a', 
+            'div', 
+            'span', 
+            'img'
+          ]
         }
       ],
-    }
+    }  
   },
-  created() {
-      for(let i = 0; i < this.Info.length; i++) {
-          this.controls.push(false);
-      }
-  },
-  methods: {
-    onInput(index, data) {
-      this.Info[index].value = data.value;
-      this.controls[index] = data.valid;
-
-      let done = 0;
-
-      for(let i = 0; i < this.Info.length; i++) {
-        if(this.controls[i]) {
-          done++;
-        }
-      }
-          this.done = done;
-    }
-  },
-  computed: {
-    progressWidth() {
-      return (100 / this.Info.length * this.done);
-    }
+  mounted() {
+    console.log(this.radioList);
   }
 }
-
 </script>
-
-<style>
-  .fa {
-    font-family: "Solid";
-  }
-  .fa-exclamation-circle {
-    color: red;
-  }
-  .fa-check-circle {
-    color: green;
-  }
-  input, label, form {
-    display: block;
-  }
-  input {
-    width: 100%;
-  }
-  form {
-    max-width: 400px;
-    margin: 0 auto;
-  }
-</style>
---
+++ src/components/Radio.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  div
+    h1 {{ title }}
+    ul
+      li(v-for="item in answers")
+        input(:type="type" :value="item" v-model="checked" v-if="type == 'radio'") 
+        input(:type="type" :value="item" v-model="selectedUsers" v-else)
+        | {{ item }} 
+</template>
+
+<script>
+export default {
+  props: ['type', 'title', 'answers'],
+  data() {
+    return {
+      checked: '',
+      selectedUsers: []
+    }
+  },
+  computed: {
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+ul
+  list-style: none
+</style>
